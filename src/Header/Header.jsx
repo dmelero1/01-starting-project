4feffcc3b37f224f0516b7c186bed8e86fe57e76
dmelero1 <danielmm1103@gmail.com>
2025-01-31 12:11:41 +0100

    Initial commit

---
+++ src/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import image from "../assets/investment-calculator-logo.png";
+function Header() {
+  return;
+  <div id="header">
+    <img src="../assets/investment-calculator-logo.png" alt="" />
+    <h1>Investment calculator</h1>
+  </div>;
+}
+
+export default Header;
